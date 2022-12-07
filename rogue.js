f8cca886bab3d41d705d5de1184c48b5de4ea9c8
Linus Banta <linus.banta.25@bishops.com>
2022-12-07 13:30:52 -0800

    Added enemies w/ movement(no damage, can stack)

--- rogue.js
+++ rogue.js
@@ -1,8 +1,91 @@
-var char = [0, 0, 30, 30];
-var moveSpeed = 30;
-var moveDelay = 150;
+var blockSize = 30;
+var char = [0, 0, blockSize, blockSize];
+var moveDelay = 300;
 var lastMoved = 0;
+var playerHealth = 10;
 var moveKeys = ['w', 'a', 's', 'd', 'arrowup', 'arrowleft', 'arrowdown', 'arrowright'];
+var enemies = []
+
+class Enemy{
+
+  constructor(health, damage, range, x1, y1){
+    this.health = health;
+    this.damage = damage;
+    this.range = range * blockSize;
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x1 + blockSize;
+    this.y2 = y1 + blockSize;
+  }
+
+  takeAction(){
+    //Probably do some stuff to detect where they would need to move, and if they don,t attack the player
+    if (this.x1 < char[0])
+    {
+      
+      console.log('detecting the location is left')
+      if (this.y1 != char[1])
+      {
+        this.x1 += blockSize;
+        this.x2 += blockSize;
+      }
+      else if (char[0] - this.x1 > this.range)
+      {
+        this.x1 += blockSize;
+        this.x2 += blockSize;
+      }
+      else
+      {
+        this.attack('right');
+      }
+    }
+    else if (this.x1 > char[0])
+    {
+      console.log('detecting the location is right')
+      if (this.x1 - char[0] <= this.range && this.y1 == char[1])
+      {
+        this.attack('left');
+      }
+      else
+      {
+        this.x1 -= blockSize;
+        this.x2 -= blockSize;
+      }
+    }
+    else if (this.y1 < char[1])
+    {
+      console.log('detecting the location is above')
+      if (char[1] - this.y1 <= this.range && this.x1 == char[0])
+      {
+        this.attack('down');
+      }
+      else
+      {
+        this.y1 += blockSize;
+        this.y2 += blockSize;
+      }
+    }
+    else if (this.y1 > char[1])
+    {
+      console.log('detecting the location is down')
+      if (this.y1 - char[1] <= this.range && this.x1 == char[0])
+      {
+        this.attack('up');
+      }
+      else
+      {
+        this.y1 -= blockSize;
+        this.y2 -= blockSize;
+      }
+    }
+
+  }
+  attack(direction)
+  {
+    //attack the player, only called if within range
+    console.log('ATTACK %s', direction)
+  }
+}
 
 function myKeyDown(event)
 /*
@@ -28,45 +111,61 @@ Inputs: the key that was pressed
 Returns: None, moves the main character
 */
 {
-  time = new Date();
-  //limits how frequent you can move
-  if (time.getTime() - moveDelay > lastMoved)
+  //moves the character
+  if(direction == "w" || direction == "arrowup")
   {
-    lastMoved = time.getTime();
-    //moves the character
-    if(direction == "w" || direction == "arrowup")
+    if(char[1] > 0)
     {
-      if(char[1] > 0)
-      {
-        char[1] -= moveSpeed;
-        char[3] -= moveSpeed;
-      }
-    }
-    else if(direction == "a" || direction == "arrowleft")
-    {
-      if(char[0] > 0)
-      {
-        char[0] -= moveSpeed;
-        char[2] -= moveSpeed;
-      }
-    }
-    else if(direction == "s" || direction == "arrowdown")
-    {
-      if(char[3] < canvas.height)
-      {
-        char[1] += moveSpeed;
-        char[3] += moveSpeed;
-      }
-    }
-    else if (direction == "d" || direction == "arrowright")
-    {
-      if(char[2] < canvas.width)
-      {
-        char[0] += moveSpeed;
-        char[2] += moveSpeed;
-      }
+      char[1] -= blockSize;
+      char[3] -= blockSize;
     }
   }
+  else if(direction == "a" || direction == "arrowleft")
+  {
+    if(char[0] > 0)
+    {
+      char[0] -= blockSize;
+      char[2] -= blockSize;
+    }
+  }
+  else if(direction == "s" || direction == "arrowdown")
+  {
+    if(char[3] < canvas.height)
+    {
+      char[1] += blockSize;
+      char[3] += blockSize;
+    }
+  }
+  else if (direction == "d" || direction == "arrowright")
+  {
+    if(char[2] < canvas.width)
+    {
+      char[0] += blockSize;
+      char[2] += blockSize;
+    }
+  }
+}
+
+function runEnemies(listOfEnemies, screen)
+{
+  time = new Date();
+  time = time.getTime();
+  if (time - lastMoved > moveDelay)
+  {
+    lastMoved = time
+    var i, enemy;
+    for (i=0; i < listOfEnemies.length; i++)
+    {
+      enemy = listOfEnemies[i];
+      enemy.takeAction();
+    }
+  }
+  for (i=0; i < listOfEnemies.length; i++)
+  {
+    enemy = listOfEnemies[i];
+    screen.fillRect(enemy.x1, enemy.y1, enemy.x2 - enemy.x1, enemy.y2 - enemy.y1);
+  }
+  
 }
 
 function drawAll()
@@ -80,6 +179,7 @@ function drawAll()
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.fillStyle = "#000000";
   context.fillRect(char[0], char[1], char[2] - char[0], char[3] - char[1]);
+  runEnemies(enemies, context);
   context.stroke();
   //loop the animation
   window.requestAnimationFrame(drawAll);
@@ -97,6 +197,10 @@ canvas.style.border = "1px solid black";
 
 //set up the context for the animation
 context = canvas.getContext("2d");
+firstEnemy = new Enemy(5, 2, 1, 90, 240);
+secondEnemy = new Enemy(5, 2, 1, 90, 0);
+enemies.push(firstEnemy)
+enemies.push(secondEnemy)
 
 //allow for keybinds
 document.addEventListener("keydown", myKeyDown);
